65f8e21b580686041457cd269b306d36290ff133
Binaya764 <bigyanlimbu179@gmail.com>
2026-05-13 20:04:47 +0545

    express and port setup

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`server is running at ${PORT}`);
+
+});
